268fae201be28d2f264f42ade63a7cef317caac7
humma-irshad <hummairshad48@gmail.com>
2022-09-25 23:06:19 +0530

    useState

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,46 +1,30 @@
-import { useReducer } from "react";
-
-const initialInputState = {
-  value: "",
-  isTouched: false,
-};
-
-const inputStateReducer = (state, action) => {
-  if (action.type === "INPUT") {
-    return { value: action.value, isTouched: state.isTouched };
-  }
-  if (action.type === "BLUR") {
-    return { value: state.value, isTouched: true };
-  }
-  if (action.type === "RESET") {
-    return { value: "", isTouched: false };
-  }
-  return initialInputState;
-};
+import { useState } from "react";
 
 const useInput = (validateValue) => {
-  const [inputState, dispatchInput] = useReducer(
-    inputStateReducer,
-    initialInputState
-  );
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
 
-  const valueIsValid = validateValue(inputState.value);
-  const hasError = !valueIsValid && inputState.isTouched;
+  // a generic way to validate the input
+  const valueIsValid = validateValue(enteredValue); // function execution
+  
+  const hasError = !valueIsValid && isTouched;
 
+  // a way to change these states
   const valueChangeHandler = (event) => {
-    dispatchInput({ type: "INPUT", value: event.target.value });
+    setEnteredValue(event.target.value);
   };
 
   const inputBlurHandler = () => {
-    dispatchInput({ type: "BLUR" });
+    setIsTouched(true);
   };
 
   const reset = () => {
-    dispatchInput({ type: "RESET" });
+    setEnteredValue("");
+    setIsTouched(false);
   };
 
   return {
-    value: inputState.value,
+    value: enteredValue,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
